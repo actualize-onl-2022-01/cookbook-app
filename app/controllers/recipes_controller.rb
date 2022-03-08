8a6b42fc804ebbbfb59053678d0276e9a6649d04
brian <riceb53@gmail.com>
2022-03-08 11:26:08 -0800

    render recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
   def initial_recipe
-    render json: {message: "here is the first recipe"}
+    the_recipe = Recipe.first
+    render json: {
+      id: the_recipe.id, 
+      title: the_recipe.title, 
+      ingredients: the_recipe.ingredients, 
+      directions: the_recipe.directions, 
+      prep_time: the_recipe.prep_time, 
+      image_url: the_recipe.image_url, 
+      chef: the_recipe.chef
+    }
   end
 end
