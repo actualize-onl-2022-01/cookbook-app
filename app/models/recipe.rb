f2527d932b355ba5a032eb7d387188537a9dbcf5
brian <riceb53@gmail.com>
2022-03-08 11:12:16 -0800

    add recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,2 @@
-class Recipe
-  attr_accessor :title, :chef, :prep_time, :ingredients, :directions
-
-  def initialize(options)
-    @title = options[:title]
-    @chef = options[:chef]
-    @prep_time = options[:prep_time]
-    @ingredients = options[:ingredients]
-    @directions = options[:directions]
-  end
+class Recipe < ApplicationRecord
 end
-
