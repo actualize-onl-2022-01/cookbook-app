a3c856101d2da80849ea13863035b5336cbd1a8c
brian <riceb53@gmail.com>
2022-04-25 11:30:24 -0700

    add recipes show test

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -36,4 +36,48 @@ RSpec.describe "Recipes", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  describe "GET /recipes/:id" do
+    it 'should return one recipe' do
+      user = User.new(
+        email: "mary@test.com",
+        password: "password",
+        name: "mary"
+      )
+      user.save! 
+
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      
+      # make a new user
+      # make a new recipe
+      recipe = Recipe.create!(
+        title: "enchiladas",
+        chef: "brian",
+        ingredients: "tortillas and meat and lettuce and sauce",
+        directions: "add the ingredients together",
+        prep_time: 14,        
+        image_url: "http://enchiladas.com/enchilada",
+        user_id: user.id,    
+      )
+      # make a web request to see the recipe
+      get "/recipes/#{recipe.id}.json", headers: {"Authorization" => "Bearer #{jwt}"}
+      recipe = JSON.parse(response.body)
+      # p recipe
+      # test out the data in the recipe
+      expect(recipe['title']).to eq('enchiladas')
+      expect(recipe['chef']).to eq('brian')
+      expect(recipe['ingredients']).to eq('tortillas and meat and lettuce and sauce')
+      expect(recipe['directions']).to eq('add the ingredients together')
+      expect(recipe['prep_time']).to eq(14)
+      expect(recipe['image_url']).to eq('http://enchiladas.com/enchilada')
+      expect(recipe['user_id']).to eq(user.id)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
